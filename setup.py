fe2e70c9afe287432a2f5300b6537d939c547bba
moasakthi <moa_sakthi@live.in>
2015-11-29 23:12:40 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,48 @@
+from distutils.core import setup
+
+setup(name='counting_leave',
+      version='1.1',
+      description="Counting Leave is date utility functions to extend python's datetime",
+      author='Siva Sakthi Velan',
+      py_modules=['counting_leave'],
+      url='http://github.com/ogt/workdays',
+      download_url = 'http://github.com/ogt/workdays/tarball/master',
+      author_email='moa_sakthi@live.in',
+      keywords = ['python', 'datetime', 'leave', 'excel' ],
+      classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+          ],
+      long_description = """
+    Extend python datetime with excel-like workday addition/subtraction
+    functionality:
+
+    COUNT_LEAVE(from_date,to_date,holidays)
+
+    Returns the number of whole working days between from_date and
+    to_date (inclusive of both from_date and to_date). Working days
+    exclude weekends and any dates identified in holidays. Use NETWORKDAYS
+    to calculate employee benefits that accrue based on the number of
+    days worked during a specific term.
+
+    COUNT_WORKDAY(from_date,days,[holidays])
+
+    Returns a number that represents a date that is the indicated number
+    of working days before or after a date (the starting date). Working
+    days exclude weekends and any dates identified as holidays. Use
+    WORKDAY to exclude weekends or holidays when you calculate invoice
+    due dates, expected delivery times, or the number of days of work
+    performed.
+
+    This module has similarities with the BusinessHours module - you may
+    want to check it out as well to see which one better fits your needs.
+""",
+      )
+
